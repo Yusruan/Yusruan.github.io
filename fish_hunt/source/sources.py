be7f85d3406a4fd24aa51ebb79b9d9787610c247
Yusruan <rispolijuan@gmail.com>
2023-05-02 08:00:16 -0500

    Configurar fish hunt

--- fish_hunt/source/sources.py
+++
@@ -1,6 +0,0 @@
-from sys import exit as wexit  #.......Finally stopping the program wheen indicated by the user
-from math import sqrt #.................Calculating bullet's x and y speed based on pelican and boat's position
-from random import randint as random #..New fish, boat and bullets position/direction and fish species selection
-from os import listdir as ls
-
-from source.imports.text_outline import * #....Creating text surfaces with outline
